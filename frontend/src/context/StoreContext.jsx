d756f1f0710fbe5402fb785280f4437df4df24d5
tagorejeet <tagorejeet2409@gmail.com>
2025-06-26 15:22:08 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems, setCartItems] = useState({})
-    const url = "http://localhost:4000"
+    const url = "https://chefs-place-cloud-project.onrender.com"
     const [token,setToken] = useState("");
 
     //we are making this so that the food menu items are called from the database and not from the assets folder.
